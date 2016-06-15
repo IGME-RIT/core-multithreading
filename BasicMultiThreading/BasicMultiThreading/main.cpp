5ca6c22e5ee6ccec3b233d1b01b93328d81f59cd
Sean Connolly <seanconnolly@mail.rit.edu>
2016-06-15 12:04:05 -0400

    Added another link.
    This was added to both the yml file and to main.cpp.

--- BasicMultiThreading/BasicMultiThreading/main.cpp
+++ BasicMultiThreading/BasicMultiThreading/main.cpp
@@ -28,8 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	//http://stackoverflow.com/questions/15148057/what-does-stdthread-join-do
 	//http://www.cplusplus.com/reference/thread/thread/detach/
 	//https://en.wikipedia.org/wiki/Critical_section
-	//http://stackoverflow.com/questions/22803600/when-should-i-use-stdthreaddetach
-	//http://stackoverflow.com/questions/22803600/when-should-i-use-stdthreaddetach
+
+//For Multithreading definitions:
+	//http://www.bogotobogo.com/cplusplus/multithreaded.php
 
 #include "ClassDeclarations.h"
 
